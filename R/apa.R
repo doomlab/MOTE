5d324f38d572a94117664a0942ad8703d1f9ac53
The Doom Lab <buchananlab@gmail.com>
2017-07-21 19:30:15 -0400

    Update and rename Apa.R to apa.R
    Updated function to format decimals with the exact number of decimals (i.e. always show requested decimals even if they are 0.010), allowing the user to pick if they want the leading zero or not.

---
+++ R/apa.R
@@ -0,0 +1,38 @@
+#' APA
+#'
+#' A function that formats decimals and leading zeroes 
+#'
+#' @param value input the number you want to format
+#' @param decimals input the number of decimals
+#' @param leading input logical TRUE for leading zeroes on decimals and FALSE for no leading zeroes on decimals
+#' @keywords APA, decimals, formatting
+#' @export
+#' @examples
+#' Apa(x,k)
+
+apa <- function(value, decimals = 3, leading = T) {
+  if (leading == T) {
+    format(value, digits = decimals, nsmall = decimals)
+    }
+  if (leading == F) {
+    sub("^(-?)0.", "\\1.", sprintf(paste("%.", decimals, "f", sep = ""), value))
+    } 
+
+##get rid of this function after updating all to the list output   
+  p.value <- function(p, k) {
+  if (k <= 2) { 
+    if (p < .01) {
+      pout <- "p < .01"
+    } else { 
+      pout = paste("p = ", Apa(p, k))
+    }
+  }
+  if (k > 2) { 
+    if (p < .001) {
+      pout <- "p < .001"
+    } else { 
+      pout <- paste("p = ", Apa(p, k))
+    }
+  }
+  return(pout)
+}
