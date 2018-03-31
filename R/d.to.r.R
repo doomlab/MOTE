8e57848418fc2fd1ced3952853d6761f3da4d45e
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-03-31 14:17:28 -0400

    update to deal with negative d values

--- R/d.to.r.R
+++ R/d.to.r.R
@@ -31,7 +31,7 @@ d.to.r <- function (d, n1, n2, a = .05) {
   correct = (n1 + n2)^2 / (n1*n2)
   n = n1 + n2
   r <- d / sqrt(d^2 + correct)
-  rsq <- r ^ 2
+  rsq <- (r) ^ 2
   se <- sqrt(4 * rsq * ((1 - rsq) ^ 2) * ((n - 3) ^ 2) / ((n ^ 2 - 1) * (3 + n)))
   t <- r / sqrt((1 - rsq) / (n - 2))
   Fvalue <- t ^ 2
@@ -40,12 +40,21 @@ d.to.r <- function (d, n1, n2, a = .05) {
   ncpboth <- conf.limits.ncf(Fvalue, df.1 = dfm, df.2 = dfe, conf.level = (1 - a))
   rsqlow <- ncpboth$Lower.Limit / (ncpboth$Lower.Limit + dfm + dfe + 1)
   rsqhigh <- ncpboth$Upper.Limit / (ncpboth$Upper.Limit + dfm + dfe + 1)
-  ciforr <- ci.R(R = r, df.1 = dfm, df.2 = dfe, conf.level = (1 - a))
+  ciforr <- ci.R(R = abs(r), df.1 = dfm, df.2 = dfe, conf.level = (1 - a))
   p <- pf(Fvalue, dfm, dfe, lower.tail = F)
   
+  #deal with negative r / d values 
+  if (r < 0) { 
+    rlow = 0 - ciforr$Lower.Conf.Limit.R
+    rhigh = 0 - ciforr$Upper.Conf.Limit.R
+  } else {
+    rlow = ciforr$Lower.Conf.Limit.R
+    rhigh = ciforr$Upper.Conf.Limit.R
+    }
+  
   output = list("r" = r, #r stats
-                "rlow" = ciforr$Lower.Conf.Limit.R, 
-                "rhigh" = ciforr$Upper.Conf.Limit.R, 
+                "rlow" = rlow, 
+                "rhigh" = rhigh, 
                 "R2" = rsq, #R squared stats
                 "R2low" = rsqlow,
                 "R2high" = rsqhigh,
