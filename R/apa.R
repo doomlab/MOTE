a1d83a40ba8945758a094b58fcb0d8ca18ce091e
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-06-20 23:48:53 -0400

    updating apa function

--- R/apa.R
+++ R/apa.R
@@ -1,31 +1,34 @@
-#' apa
+#' APA Format
 #'
-#' A function that formats decimals and leading zeroes 
+#' A function that formats decimals and leading zeroes
+#' for creating reports in scientific style.
 #'
-#' @param value the number you want to format
-#' @param decimals the number of decimal points desired
-#' @param leading logical TRUE for leading zeroes on decimals and FALSE for no leading zeroes on decimals
+#' @param value A set of numeric values, either a single number, vector, or set of columns.
+#' @param decimals The number of decimal points desired in the output.
+#' @param leading Logical value: \code{TRUE} for leading zeroes on decimals
+#'    and \code{FALSE} for no leading zeroes on decimals. The default is \code{TRUE}.
 #' @keywords APA, decimals, formatting
-#' @export
+#' @details
+#'   This function creates "pretty" character vectors from numeric variables
+#'   for printing as part of a report. The value can take a single number,
+#'   matrix, vector, or multiple columns from a data frame, as long as they are
+#'   numeric. The values will be coerced into numeric if they are characters or
+#'   logical values, but this process may result in an error if values are
+#'   truly alphanumeric.
 #' @examples
 #' apa(value = 0.54674, decimals = 3, leading = T)
+#' @export
 
 apa <- function(value, decimals = 3, leading = T) {
-  # Function creates printable results from output
-  #
-  # Args:
-  #   value     : the number you want to format
-  #   decimals  : the number of decimal points desired
-  #   leading   : T for leading zeroes, F for no leading zeroes
-  #
-  # Returns:
-  #   Character of formatted values
-  
+
   if (leading == T) {
-    formnumber <- format(round(value, decimals), digits = decimals, nsmall = decimals)
+    formnumber <- format(round(as.numeric(value), decimals), digits = decimals, nsmall = decimals)
     }
   if (leading == F) {
-    formnumber <- sub("^(-?)0.", "\\1.", sprintf(paste("%.", decimals, "f", sep = ""), value))
-    } 
+    formnumber <- sub("^(-?)0.", "\\1.", sprintf(paste("%.", decimals, "f", sep = ""), as.numeric(value)))
+    }
   return(formnumber)
   }
+
+#' @rdname apa
+#' @export
