f49c815a75014826cd96ce94e16d99561a6af026
Amber Gillenwaters <gillenwaters17@live.missouristate.edu>
2018-10-24 16:12:02 -0500

    quick fix

--- R/d.single.t.R
+++ R/d.single.t.R
@@ -34,19 +34,19 @@
 #' @export
 #' @examples
 #'
-#' The following example is derived from the "singt_data" dataset included
-#' in the MOTE library.
+#' #The following example is derived from the "singt_data" dataset included
+#' #in the MOTE library.
 #'
 #' #A school has a gifted/honors program that they claim is
-#' significantly better than others in the country. The gifted/honors
-#' students in this school scored an average of 1370 on the SAT,
-#' with a standard deviation of 112.7, while the national average
-#' for gifted programs is a SAT score of 1080.
+#' #significantly better than others in the country. The gifted/honors
+#' #students in this school scored an average of 1370 on the SAT,
+#' #with a standard deviation of 112.7, while the national average
+#' #for gifted programs is a SAT score of 1080.
 #'
 #'     gift = t.test(singt_data, mu = 1080, alternative = "two.sided")
 #'
-#' You can type in the numbers directly as shown below,
-#' or refer to your dataset within the function.
+#' #You can type in the numbers directly as shown below,
+#' #or refer to your dataset within the function.
 #'
 #'     d.single.t(m = 1370, u = 1080, sd = 112.7, n = 14, a = .05)
 #'
