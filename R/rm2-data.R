8f797cd7d7a51626f2f032cc812cc6e3b71b6d7f
Amber Gillenwaters <34293640+Gillenwaters@users.noreply.github.com>
2019-01-30 13:02:47 -0600

    datasets

---
+++ R/rm2-data.R
@@ -0,0 +1,27 @@
+#' Repeated Measures Oneway ANOVA Example Data
+#'
+#' Dataset for use in \code{\link{omega.F.R}}.
+#' Participants were tested over several days to measure
+#' variations in their pulse given different types of stimuli.
+#' One stimulus was a neutral picture (like a toaster),
+#' while other stimuli were cute/happy pictures (puppies, babies),
+#' and negative stimuli (mutilated faces, pictures of war).
+#' Were there differences in pulse for each participant
+#' across the stimuli?
+#'
+#' @docType data
+#'
+#' @usage data(rm1_data)
+#'
+#' @format A data frame including ratings toward pictures.
+#'
+#' neutral: pulse during exposure to neutral stimuli
+#' positive: pulse during exposure to positive stimuli
+#' negative: pulse during exposure to negative stimuli
+#'
+#' @keywords datasets
+#'
+#' @references Nolan and Heizen Statistics for the Behavioral Sciences
+#' (\href{https://www.macmillanlearning.com/Catalog/product/statisticsforthebehavioralsciences-rentalonly-fourthedition-nolan}{Book Link})
+#'
+"rm1_data"
