3f445c16551678b6d1face547b51a8038f0662ab
The Doom Lab <buchananlab@gmail.com>
2025-11-26 10:15:12 -0600

    rename as setup and ignore

---
+++ setup/setup.R
@@ -0,0 +1,28 @@
+library(devtools)
+library(usethis)
+
+# usethis::use_build_ignore(c("setup"))
+# usethis::use_build_ignore(c("LICENSE"))
+
+#usethis::build_readme()
+#usethis::use_data(mirtdata, overwrite = T)
+#usethis::use_tutorial("irt", "Item Response Theory", open = interactive())
+#usethis::use_vignette("Package-Tutorial",
+#                       "Visualize MI Tutorial")
+
+
+library(roxygen2)
+roxygenize()
+devtools::check()
+tools::buildVignettes(dir = ".", tangle=TRUE)
+
+#dir.create("inst")
+dir.create("inst/doc")
+file.copy(dir("vignettes", full.names=TRUE), "inst/doc", overwrite=TRUE)
+
+library(pkgdown)
+# usethis::use_pkgdown()
+pkgdown::build_site()
+# usethis::use_pkgdown_github_pages()
+
+devtools::install_github("doomlab/MOTE")
